c4f380ece9d66c222c0f1afd1678e06d9e035525
hadar <hadar01@kazoula.com>
2026-09-06 21:06:01 +0300

    Add Header component, update title and favicon, style header with CSS class

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <h1>הגינה שלי</h1>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import Header from './Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* <App /> */}
+<h1 className="header-title">הגינה שלי</h1> 
+
+ </StrictMode>,
+ 
+  
 )
